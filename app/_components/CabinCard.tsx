5e87db466ab2d64131258c828bb9859e88d96aed
hossein <hosinrg1380@gmail.com>
2024-09-01 01:42:26 +0330

    complete nested route in cabin

--- app/_components/CabinCard.tsx
+++ app/_components/CabinCard.tsx
@@ -1,15 +1,29 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
 import Image from 'next/image';
-function CabinCard({ cabin }) {
-    const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
+import Link from "next/link";
 
+interface CabinType {
+    id: string;
+    name: string;
+    maxcapacity: number;
+    regularprice: number;
+    discount: number;
+    image: string;
+}
+
+function CabinCard({ cabin }: { cabin: CabinType }) {
+    const { id, name, maxcapacity, regularprice, discount, image } = cabin;
     return (
         <div className="flex border-primary-800 border">
-            <Image
-                src={image}
-                alt={`Cabin ${name}`}
-                className="flex-1 border-r border-primary-800"
-            />
+            <div className="flex-1 relative">
+                <Image
+                    objectFit="cover"
+                    fill
+                    src={image}
+                    alt={`Cabin ${name}`}
+                    className="flex-1 border-r border-primary-800"
+                />
+            </div>
 
             <div className="flex-grow">
                 <div className="pt-5 pb-4 px-7 bg-primary-950">
@@ -20,7 +34,7 @@ function CabinCard({ cabin }) {
                     <div className="flex gap-3 items-center mb-2">
                         <UsersIcon className="h-5 w-5 text-primary-600" />
                         <p className="text-lg text-primary-200">
-                            For up to <span className="font-bold">{maxCapacity}</span> guests
+                            For up to <span className="font-bold">{maxcapacity}</span> guests
                         </p>
                     </div>
 
@@ -28,26 +42,26 @@ function CabinCard({ cabin }) {
                         {discount > 0 ? (
                             <>
                                 <span className="text-3xl font-[350]">
-                                    ${regularPrice - discount}
+                                    ${regularprice - discount}
                                 </span>
                                 <span className="line-through font-semibold text-primary-600">
-                                    ${regularPrice}
+                                    ${regularprice}
                                 </span>
                             </>
                         ) : (
-                            <span className="text-3xl font-[350]">${regularPrice}</span>
+                            <span className="text-3xl font-[350]">${regularprice}</span>
                         )}
                         <span className="text-primary-200">/ night</span>
                     </p>
                 </div>
 
                 <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-                    <a
+                    <Link
                         href={`/cabins/${id}`}
                         className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
                     >
                         Details & reservation &rarr;
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,18 +1,13 @@
-import CabinCard from "@/app/_components/CabinCard";
+'use cilent'
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
     title: 'Cabins'
 }
 
 function page() {
-    const cabins: Array<{ id: number }> = [
-        { id: 1, name: 'rope', maxCapacity: '5', regularPrice: 120, discount: 22, image: '' },
-        { id: 2 },
-        { id: 3 },
-        { id: 4 },
-        { id: 5 },
-        { id: 6 }
-    ];
     return <div>
         <h1 className="text-4xl mb-5 text-accent-400 font-medium">
             Our Luxury Cabins
@@ -25,14 +20,9 @@ function page() {
             away from home. The perfect spot for a peaceful, calm vacation. Welcome
             to paradise.
         </p>
-
-        {cabins.length > 0 && (
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                {cabins.map((cabin) => (
-                    <CabinCard cabin={cabin} key={cabin.id} />
-                ))}
-            </div>
-        )}
+        <Suspense fallback={<Spinner />}>
+            <CabinList />
+        </Suspense>
     </div>;
 }
 
